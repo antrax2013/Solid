8051d80766d3f4c9e1ce6b10d90aec3790971fb6
antrax <antrax2013@hotmail.com>
2025-10-13 07:28:40 +0200

    Ajoutez des fichiers projet.

---
+++ Src/Sdk/Result.cs
@@ -0,0 +1,41 @@
+﻿namespace Sdk
+{
+    public class Result
+    {
+        private readonly bool _success;
+        private readonly string? _message;
+        public bool IsSuccess => _success;
+        public bool IsFailure => !_success;
+
+        public string? Message => _message;
+
+        private Result(bool success, string? message)
+        {
+            _success = success;
+            _message = message;
+        }
+
+        public static Result Ok() => new(true, null);
+        public static Result Fail(string message) => new(false, message);
+    }
+
+    public class Result<T>
+    {
+        private readonly Result _result;
+        private readonly T? _value;
+
+        private Result(T? value, bool success, string? message)
+        {
+            _result = success ? Result.Ok() : Result.Fail(message ?? throw new ArgumentException("message is null"));
+            _value = value;
+        }
+
+        public bool IsSuccess => _result.IsSuccess;
+        public bool IsFailure => _result.IsFailure;
+        public T? Value => _value;
+
+        public static Result<T> Ok(T value) => new(value, true, null);
+        public static Result<T> Fail(string message) => new(default, false, message);
+
+    }
+}
---
+++ Tests/SingleResponsabilityPrinciple.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using NFluent;
+using Sdk;
+
+namespace SingleResponsabilityPrinciple.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public async Task Quand_Je_Sauvegarde_Et_Recharge_Un_Livre_Depuis_Un_Fichier_Alors_Ses_Proprietes_Sont_Identiques()
+        {
+            //Given 
+            Book book = new("Mon titre", "John Doe", "978-2290415634", 448, new DateTime(2025, 10, 11));
+
+            //When
+            Result saveOperation = await book.SaveToFile("./");
+            Check.That(saveOperation.IsSuccess).IsTrue();
+
+            Result<Book> readOperation = await book.LoadFromFile("./");
+            Check.That(readOperation.IsSuccess).IsTrue();
+            Check.That(readOperation.Value).HasFieldsWithSameValues(book);
+        }
+    }
+}
